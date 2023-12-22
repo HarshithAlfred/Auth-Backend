44a1791a47139a9f3640493be279c67257e2b285
Harshith Alfred <harshithalfred@gmail.com>
2023-12-22 08:54:24 +0530

    small change2

--- Routes/chat.js
+++ Routes/chat.js
@@ -1,30 +1,34 @@
-const express=require("express")
-const http =require("http")
-const dotenv=require("dotenv")
-dotenv.config()
-const socket=require("socket.io")
-const cors=require("cors")
-const app=express()
-const server=http.createServer(app)
-try{
-const io= socket(server,{
-    cors: {
-        origin: process.env.frontend ,
-        methods: ["GET", "POST"]
-      }
-});
+const express = require("express");
+const http = require("http");
+const dotenv = require("dotenv");
+dotenv.config();
+const { Server } = require("socket.io");
+const cors = require("cors");
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
 app.use(cors());
-io.on("connection",(socket)=>{
-    console.log("user connected")
-    socket.on('chat message',(data)=>{
-        console.log(`message from : ${data.name} : ${data.message}`)
-    io.emit("chat message",data)
+try{
+io.on("connection", (socket) => {
+    console.log("user connected");
+
+    socket.on('chat message', (data) => {
+        console.log(`message from : ${data.name} : ${data.message}`);
+        io.emit("chat message", data);
     });
-    socket.on("disconnect",()=>{
-        console.log("user disconnected")
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
     });
-});}
-catch(e){
-    console.log("port 4000 error",e)
+});
+
+const PORT = process.env.PORT || 4001;
+
+server.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+});
 }
-server.listen(4001,()=>{console.log("Server running on port:4001")});
+catch(e){
+    console.log("port 4001 error",e)
+}
