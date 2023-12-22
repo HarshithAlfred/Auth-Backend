d76e448a032bc14f299283119f83f1aea060e6e9
Harshith Alfred <harshithalfred@gmail.com>
2023-12-22 10:11:41 +0530

    changes

--- Routes/chat.js
+++ Routes/chat.js
@@ -6,7 +6,12 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server,{
+    cors: {
+      origin: process.env.frontend,
+      methods: ["GET", "POST"]
+    }
+  });
 
 app.use(cors());
 try{
