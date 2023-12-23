0bd6ea80303b4a730f57559b1b61884dc1617243
Harshith Alfred <harshithalfred@gmail.com>
2023-12-23 08:30:12 +0530

    test 1

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const chat=require("./Routes/chat.js")
 const app=express();
 const dotenv= require("dotenv")
 dotenv.config()
+const http = require("http");
+const { Server } = require("socket.io");
+const server = http.createServer(app);
 app.use(express.json());
 app.use(cors({origin:process.env.frontend}));
 const port=4000;
@@ -21,3 +24,33 @@ app.use('/login',login);
 app.use('/home',home);
 app.use('/changepass',chanegapss);
 //app.use('/chat',chat)
+
+const io = new Server(server,{
+    path: '/socket.io',
+    cors: {
+      origin: process.env.frontend,
+      methods: ["GET", "POST"]
+    }
+  });
+try{
+io.on("connection", (socket) => {
+    console.log("user connected");
+    console.log("inside",socket);
+    socket.on('chat message', (data) => {
+        console.log(`message from : ${data.name} : ${data.message}`);
+        io.emit("chat message", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
+});
+
+const PORT =4001;
+server.listen(PORT, () => {
+    console.log(`Server running on port:${PORT}`);
+});
+}
+catch(e){
+    console.log("port 4001 error",e)
+}
